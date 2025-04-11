740df44d647455080fd1eb61c6f860b4ea58c13b
Andruxa <kazak1995kazak@yandex.ru>
2025-04-11 20:02:27 +0300

    2.3.2. Написание кода

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float speed = 3.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
