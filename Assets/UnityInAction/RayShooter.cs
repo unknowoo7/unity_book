d1810b99086733b808fb52b08eadf703c6f707fe
Andruxa <kazak1995kazak@yandex.ru>
2025-04-12 17:34:18 +0300

    Create sphere indicator hit

--- Assets/UnityInAction/RayShooter.cs
+++ Assets/UnityInAction/RayShooter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace UnityInAction
@@ -16,14 +17,22 @@ namespace UnityInAction
     {
       if (Input.GetMouseButtonDown(0))
       {
-        Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+        Vector3 point = new Vector3(_camera.pixelWidth / 2.0f, _camera.pixelHeight / 2.0f, 0);
         Ray ray = _camera.ScreenPointToRay(point);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-          Debug.Log("Hit" + hit.point);
+          StartCoroutine(SphereIndicator(hit.point));;
         }
       }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+      GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+      sphere.transform.position = pos;
+      yield return new WaitForSeconds(1);
+      Destroy(sphere);
     }
   }
 }
