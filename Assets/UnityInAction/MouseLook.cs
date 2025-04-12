2b4b2a82f8c683866a9d78dcef312dc4403a67fb
Andruxa <kazak1995kazak@yandex.ru>
2025-04-12 08:17:16 +0300

    2.4.2. Поворот по вертикали с ограничениями

--- Assets/UnityInAction/MouseLook.cs
+++ Assets/UnityInAction/MouseLook.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 
 namespace UnityInAction
 {
-    public class MouseLook : MonoBehaviour
+  public class MouseLook : MonoBehaviour
+  {
+    public enum RotationAxes
     {
-        public enum RotationAxes
-        {
-            MouseXAndY,
-            MouseX,
-            MouseY
-        }
-    
-        public RotationAxes axes = RotationAxes.MouseXAndY;
-    
-        void Update()
-        {
-            if (axes == RotationAxes.MouseX)
-            {
-
-            }
-            else if (axes == RotationAxes.MouseY)
-            {
-            
-            }
-            else
-            {
-            
-            }
-        }
+      MouseXAndY,
+      MouseX,
+      MouseY
     }
-    
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+
+    void Update()
+    {
+      if (axes == RotationAxes.MouseX)
+      {        
+        transform.Rotate(0,  Input.GetAxis("Mouse X") * sensitivityHor, 0);
+      }
+      else if (axes == RotationAxes.MouseY)
+      {
+        
+      }
+      else
+      {
+        
+      }
+    }
+  }
 }
