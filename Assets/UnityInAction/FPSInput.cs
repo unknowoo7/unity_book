8d8b661ca6a934c7dd0b8c907cb808f82e0755d1
Andruxa <kazak1995kazak@yandex.ru>
2025-04-12 14:51:40 +0300

    [81] 2.5.4. Ходить, а не летать

--- Assets/UnityInAction/FPSInput.cs
+++ Assets/UnityInAction/FPSInput.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace UnityInAction
@@ -5,13 +6,23 @@ namespace UnityInAction
   public class FPSInput : MonoBehaviour
   {
     public float speed = 6.0f;
+    private CharacterController characterController;
 
-    void Update()
+    private void Start()
+    {
+      characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
     {
       float deltaX = Input.GetAxis("Horizontal") * speed;
       float deltaZ = Input.GetAxis("Vertical") * speed;
-      Debug.Log(Time.deltaTime);
-      transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+      Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+      movement = Vector3.ClampMagnitude(movement, speed);
+      
+      movement *= Time.deltaTime;
+      movement = transform.TransformDirection(movement);
+      characterController.Move(movement);
     }
   }
 }
